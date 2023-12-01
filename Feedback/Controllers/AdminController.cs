dbfa23492a6d68f76738d952f296281fa8fffee2
notkapier <notkapier@gmail.com>
2023-12-01 08:02:38 +0800

    First Commit

---
+++ Feedback/Controllers/AdminController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Feedback.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Users()
+        {
+            return View();
+        }
+        public IActionResult Ratings()
+        {
+            return View();
+        }
+        public IActionResult Analysis()
+        {
+            return View();
+        }
+    }
+}
---
+++ Feedback/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Feedback.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Feedback()
+        {
+            return View();
+        }
+        public IActionResult FeedbackList()
+        {
+            return View();
+        }
+    }
+}
